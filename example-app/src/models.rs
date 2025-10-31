4b57fd969e744093e040720c474518f9bda0a6a4
Andrey Konstantinov <andrey@partly.com>
2025-10-31 01:28:53 +0000

    more clean up

--- example-app/src/models.rs
+++ example-app/src/models.rs
@@ -1,13 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-/// Example custom type for test_data field  
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct TestData {
-    pub message: String,
-    pub value: i32,
-    pub metadata: Option<serde_json::Value>,
-}
-
 /// User profile information stored as JSON in the database
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct UserProfile {
@@ -30,13 +22,4 @@ pub struct UserPreferences {
 pub struct SocialLink {
     pub platform: String,
     pub url: String,
-}
-
-/// User settings stored as JSON in the database
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct UserSettings {
-    pub privacy_level: String,
-    pub email_notifications: bool,
-    pub two_factor_enabled: bool,
-    pub api_access: bool,
 }
